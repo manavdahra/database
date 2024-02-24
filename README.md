9c1919ed02eea526227a86f504f59ba44f693ae7
Manav Dahra <manav.dahra@gmail.com>
2024-02-24 19:43:29 +0100

    Compiler and VM with readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# database
+Database - design and implementation
+==
+
+### Components
+
+### REPL
+Set up REPL which is like the front end for the database.
+This receievs commands from the user using the standard input.
+It should run infinitely until user gives command to exit.
+
+### Compiler and VM
+The "front-end" of sqlite is a SQL compiler that parses a string and outputs an internal representation called bytecode.
+This bytecode is passed to the virtual machine, which executes it.
+
+Breaking things into two steps like this has a couple advantages:
+- Reduces the complexity of each part (e.g. virtual machine does not worry about syntax errors)
+- Allows compiling common queries once and caching the bytecode for improved performance
--- main.c
+++ main.c
@@ -3,8 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct InputBuffer
-{
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_UNRECOGNISED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNISED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct Statement {
+    StatementType type;
+} Statement;
+
+typedef struct InputBuffer {
     char* buffer;
     size_t buffer_length;
     ssize_t input_length;
@@ -40,6 +58,37 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_UNRECOGNISED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNISED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
 
 int main(int argc, char const *argv[])
 {
@@ -48,12 +97,29 @@ int main(int argc, char const *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer))
+            {
+            case META_COMMAND_SUCCESS:
+                continue;
+            case META_UNRECOGNISED_COMMAND:
+                printf("Unreconised command '%s'\n", input_buffer->buffer);
+                continue;
+            }
         }
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
+        {
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNISED_STATEMENT):
+            printf("Unrecognized command '%s'\n", input_buffer->buffer);
+            continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
     return 0;
 }
